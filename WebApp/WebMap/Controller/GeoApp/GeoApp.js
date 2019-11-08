54d0d607e104056da84b4085987318815c74b1d5
gzorro@intergrupo.com <gzorro@intergrupo.com>
2019-11-08 10:16:55 -0500

    Se crea archivo global.js para manejar variables y métodos globales

--- WebApp/WebMap/Controller/GeoApp/GeoApp.js
+++ WebApp/WebMap/Controller/GeoApp/GeoApp.js
@@ -1,40 +1,4 @@
-﻿/**property */
-var map = {};
-var view = {};
-var sketch = {};
-var graphicsLayer = {};
-var graphicInstance = null;
-
-//Tipos de mapa a implementar
-let _enumTypeMaps = 
-    {
-        Topo: "topo"
-        , Satellite:"satellite"
-        , Hybrid:"hybrid"
-        , Gray:"gray"
-        , DarkGray:"dark-gray"
-        , Oceans:"oceans"
-        , Osm:"osm"
-        , NationalGeographic:"national-geographic"
-        , StreetsReliefVector: "streets-relief-vector"
-}
-//Posiciones permitidas para objeto view
-let _enumTypePosition =
-{
-   TopRight: "top-right"
-   , TopLeft: "top-left"
-   , TopLeading: "top-leading"
-   , TopTrailing: "top-trailing"
-   , BottomRight: "bottom-right"
-   , BottomLeft: "bottom-left"
-   , BottomLeading: "bottom-leading"
-   , BottomTrailing: "bottom-trailing"
-   , Manual: "manual"
-}
-//Lista de polígonos a guardar
-let _listPoligonToSave = [];
-let _listGraphicsToDelete = [];
-
+﻿
 /**
  * Clase parental de Instancia del mapa esri
  * */
