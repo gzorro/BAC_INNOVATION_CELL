a59a003bca51f450f1372661eb4428aa12f85b47
gzorro@intergrupo.com <gzorro@intergrupo.com>
2019-11-08 09:56:03 -0500

    Se agrega proyecto html WebApp a capa Web

---
+++ WebApp/WebMap/Controller/GeoApp/GeoApp.js
@@ -0,0 +1,156 @@
+﻿/**property */
+var map = {};
+var view = {};
+var sketch = {};
+var graphicsLayer = {};
+var graphicInstance = null;
+
+//Tipos de mapa a implementar
+let _enumTypeMaps = 
+    {
+        Topo: "topo"
+        , Satellite:"satellite"
+        , Hybrid:"hybrid"
+        , Gray:"gray"
+        , DarkGray:"dark-gray"
+        , Oceans:"oceans"
+        , Osm:"osm"
+        , NationalGeographic:"national-geographic"
+        , StreetsReliefVector: "streets-relief-vector"
+}
+//Posiciones permitidas para objeto view
+let _enumTypePosition =
+{
+   TopRight: "top-right"
+   , TopLeft: "top-left"
+   , TopLeading: "top-leading"
+   , TopTrailing: "top-trailing"
+   , BottomRight: "bottom-right"
+   , BottomLeft: "bottom-left"
+   , BottomLeading: "bottom-leading"
+   , BottomTrailing: "bottom-trailing"
+   , Manual: "manual"
+}
+//Lista de polígonos a guardar
+let _listPoligonToSave = [];
+let _listGraphicsToDelete = [];
+
+/**
+ * Clase parental de Instancia del mapa esri
+ * */
+class GeoApp {
+    /**
+     * Constructor de clase
+     * */
+    constructor() {
+        GeoApp.Init();
+    }
+
+    static Init() {
+        require(
+            [
+                "esri/Map"
+                , "esri/views/MapView"
+                , "esri/Graphic"
+                , "esri/layers/GraphicsLayer"
+                , "esri/widgets/Sketch"
+            ],
+            function (Map, MapView, Graphic, GraphicsLayer, Sketch) {
+                //Carga de la capa gráfica
+                graphicsLayer = new GraphicsLayer();
+
+                //Instanciamos objeto de tipo Graphics para poder usar sus métodos en las clases funcionales
+                graphicInstance = new Graphic();
+
+                //Inicialización de componentes
+                map = new Map(
+                    {
+                        basemap: _enumTypeMaps.Topo,
+                        layers: [graphicsLayer]
+                    });
+                view = new MapView(
+                    {
+                        container: "viewDiv", // Referencia al objeton DOM (div)
+                        map: map, // Referencia a objeto map
+                        center: [-74.2973328, 4.570868], // longitude, latitude //Centro de Colombia
+                        zoom: 8
+                    });
+                sketch = new Sketch(
+                    {
+                        view: view,
+                        layer: graphicsLayer
+                    });
+
+                view.ui.add(sketch, "top-right");
+
+                //Agregar componente de coordenadas
+                AddCoordsToView();
+
+                AddElementToView("button", "btnGuardar", "Guardar", ["btn", "btn-success", "hovicon effect"], _enumTypePosition.TopLeading, false);
+                AddElementToView("button", "btnCargar", "Cargar", ["btn", "btn-primary", "hovicon effect"], _enumTypePosition.TopLeading);
+
+                /**************************** Eventos Bind ****************************/
+
+                //Cuando se guarde el polígono se ejecuta este evento
+                sketch.on("create", function (event) {
+                    SavePolygon(event);
+                });
+
+                //evento complementario que guarda posición en mapa
+                view.on("pointer-move", function (evt) {
+                    ShowCoordinates(
+                        view.toMap(
+                            { x: evt.x, y: evt.y }
+                        )
+                    );
+                });
+
+                //evento de asignación de coordenadas
+                view.watch("stationary", function (isStationary) {
+                    ShowCoordinates(view.center);
+                });
+
+                //evento de guardado de polígonos
+                $('#btnGuardar').on('click', function(evt) {
+                    let namefile = `_PolygonSaved_.json`
+                    // SavePolygonList(namefile, 'text/json');
+                    debugger;
+                    SavePolygonList(namefile);
+                });
+
+                //evento de carga de polígonos
+                $('#btnCargar').on('click', function(evt) {
+                    // LoadJsonPolygon('_PolygonSaved_.json');
+                    $.getJSON('_PolygonSaved_.json', function(json) {
+                        json.forEach(x => {
+                            if(x.length > 0)
+                            {
+
+                            }
+                            let polygon = {
+                                type: "polygon",
+                                rings: x
+                              };
+                        
+                            let simpleFillSymbol = {
+                                type: "simple-fill",
+                                color: [227, 139, 79, 0.8],  // orange, opacity 80%
+                                outline: {
+                                    color: [255, 255, 255],
+                                    width: 1
+                                }
+                            };
+                            debugger;
+                            let polygonGraphic = new Graphic({
+                                geometry: polygon,
+                                symbol: simpleFillSymbol
+                              });
+                            
+                            graphicsLayer.add(polygonGraphic);
+                        });
+                    });
+                });
+            }
+        );
+    }
+}
---
+++ WebApp/WebMap/Controller/GeoApp/MainLogic.js
@@ -0,0 +1,190 @@
+﻿/**
+ * Core principal de funcionalidad del mapa geográfico
+ * @Authors {Germán f. Grimaldi}, {Javier Becerra}
+ * 
+ * */
+
+/* ******************************************************************************************************* */
+/* ******************************************************************************************************* */
+
+/**
+ * Agregar componente de coordenadas al objeto view
+ * */
+function AddCoordsToView()
+{
+    let coordsWidget = document.createElement("div");
+    coordsWidget.id = "coordsWidget";
+    coordsWidget.className = "esri-widget esri-component";
+    coordsWidget.style.padding = "7px 15px 5px";
+
+    view.ui.add(coordsWidget, "bottom-right");
+}
+
+/**
+ * Agrega determinado elemento a la vista
+ * @param {string} nameElement Nombre del elemento a agregar
+ * @param {string} idElement ID de elemento a agregar
+ * @param {string} description descripción para el elemento ó innerText
+ * @param {List<string>} classes lista de string con las clases a implementar para el elemento a agregar
+ * @param {string} ubication Ubicación del elemento a agregar según librería de esri
+ * @param {bool} isVisible determinado si el elemento debe de mostrarse inmediatamente
+ */
+function AddElementToView(nameElement, idElement, description, classes, ubication, isVisible = true) {
+     let elementHTML = document.createElement(nameElement);
+    elementHTML.id = idElement;
+    elementHTML.innerText = description;
+    let classesToImplement = "";
+    classes.forEach(x => classesToImplement += `${x} `);
+    elementHTML.className = classesToImplement;
+    elementHTML.style.padding = "11px";
+    view.ui.add(elementHTML, ubication);
+    if(!isVisible)
+        $(elementHTML).hide();
+}
+
+/**
+ * Mostrar las coordenadas en el DOM
+ * @param {any} pt punto posición
+ */
+function ShowCoordinates(pt)
+{
+    var coords = "Lat/Lon " + pt.latitude.toFixed(3) + " " + pt.longitude.toFixed(3) +
+        " | Scale 1:" + Math.round(view.scale * 1) / 1 +
+        " | Zoom " + view.zoom;
+    coordsWidget.innerHTML = coords;
+}
+
+/**
+ * Guardar coordenadas de polígono en .json
+ * @param {any} event
+ */
+function SavePolygon (event) {
+    if (event.state === "complete") {
+        let polygonCoordinate = event.graphic.geometry.rings;
+        _listPoligonToSave.push(polygonCoordinate[0]); 
+        if(_listPoligonToSave.length === 1)
+            $('#btnGuardar').show('slow');
+        _listGraphicsToDelete.push(event);
+    }
+}
+
+/**
+ * Guardar la lista de polígonos registrada en un archivo de tipo
+ * @param {string} nameFile nombre del archivo
+ * @param {string} fileType tipo de archivo (xml, json, txt)
+ */
+function SavePolygonList(nameFile, fileType)
+{
+    if(_listPoligonToSave.length > 0)
+    {
+        // let listFormatted = SetFormatDecimal(100000,9);
+        let jsonData = JSON.stringify(_listPoligonToSave);
+        // let jsonData = JSON.stringify(listFormatted);
+        Download(jsonData, nameFile, fileType);
+        PolygonsRemove();
+        $('#btnGuardar').hide('slow');
+    }else{
+        alert('No se ha registrado ningún polígono aún. A continuación registra un polígono e intenta de nuevo');
+    }
+}
+
+/**
+ * Eliminar los polígonos que ya se han guardado en el archivo plano
+ */
+function PolygonsRemove()
+{
+    _listGraphicsToDelete.forEach(x => graphicsLayer.remove(x.graphic))    
+}
+
+/**
+ * Cargar los polígonos desde un archivo json
+ * * El archivo se carga de forma fija y no tiene ninguna validación
+ * @param {string} fileName nombre del archivo
+ */
+function LoadJsonPolygon(fileName)
+{
+    try{
+        $.getJSON(fileName, function(json) {
+            DrawPolygonsSaved(json);
+        });
+    }catch{
+        alert('Ups!, algo salió mal, vuelve a intentar');
+    }
+}
+/**
+ * Dibuja en el objeto view los polígonos guardados
+ * @param {any} jsonObject 
+ */
+function DrawPolygonsSaved(jsonObject)
+{
+    debugger;
+    jsonObject.forEach(x => {
+        if(x.length > 0)
+            DrawPolygon(x);
+    });
+}
+
+/**
+ * Grafica y dibuja en el objeto view, un polígono
+ * @param {Array} polygonCoordinates coordenadas del polígono(latitud y longitud) 
+ */
+function DrawPolygon(polygonCoordinates)
+{
+    debugger;
+    let polygon = {
+        type: "polygon",
+        rings: polygonCoordinates
+      };
+
+    let simpleFillSymbol = {
+        type: "simple-fill",
+        color: [227, 139, 79, 0.5],  // orange, opacity 80%
+        outline: {
+            color: [255, 255, 255],
+            width: 1
+        }
+    };
+    let instance = graphicInstance; 
+    instance.geometry = polygon;
+    instance.symbol = simpleFillSymbol;
+
+    let polygonGraphic = instance;
+    
+    graphicsLayer.add(polygonGraphic);
+}
+
+/**
+ * Ejecuta guardado de archivo a través de una simulación de evento
+ * @param {string} content Contenido en formato determinado (.json, .xml)
+ * @param {string} fileName Nombre del archivo que se va a agurdar incluyendo formato (.json, .xml, .txt) 
+ * @param {string} contentType Typo de contenido // Default value: 'text/json'
+ */
+function Download(content, fileName, contentType = 'text/json') {
+    var a = document.createElement("a");
+    var file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+    a.remove();
+}
+
+/**
+ * Configura el formato para georeferenciación espacial en determinados valores
+ * @param {int} divisibleBy valor numérico que dividira el valor espacial
+ * @param {int} lengthFormat Longitud de valores numéricos
+ */
+function SetFormatDecimal(divisibleBy, lengthFormat)
+{
+    let listPolygonsAssigned = [];
+    _listPoligonToSave.forEach(x => {
+        let listCoordsPolygon = [];
+        x.forEach(y => {
+            let obj = [parseFloat((y[0]/divisibleBy).toFixed(lengthFormat)), parseFloat((y[1]/divisibleBy).toFixed(lengthFormat))]
+            // let obj = [parseFloat((y[1]/divisibleBy).toFixed(lengthFormat)), parseFloat((y[0]/divisibleBy).toFixed(lengthFormat))]
+            
+            listCoordsPolygon.push(obj);
+        });
+        listPolygonsAssigned.push(listCoordsPolygon);
+    });
+    return listPolygonsAssigned;
+}
