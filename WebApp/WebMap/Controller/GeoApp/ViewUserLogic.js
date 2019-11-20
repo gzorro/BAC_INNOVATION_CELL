335f7c3623176fac5118b3601e46f0bd25ef919f
javierdbecerra <jbecerrao@intergrupo.com>
2019-11-20 16:55:58 -0500

    Creación archivo corrrespondiente a la visualización de la información correspondiente a un usuario desde la base de datos de BAC

---
+++ WebApp/WebMap/Controller/GeoApp/ViewUserLogic.js
@@ -0,0 +1,23 @@
+/**
+ *  Funcionalidad busqueda dados un punto geográfico (Latitud y longitud)
+ * @Authors {Germán F. Grimaldi}, {Javier Becerra}
+ */
+
+/* ******************************************************************************************************* */
+/* ******************************************************************************************************* */
+
+
+
+/** 
+ * Visualiza la información correspondiente a un cliente en particular
+ * @param person Objeto con la información especifica por cliente a cargar
+ * 
+ **/
+
+function ViewProfileUser(person){
+
+     //Función crear modal
+     createModal();
+     
+
+}
